d7fea01a92bf2a118a0274845633c35237397a34
Husnain Mahmood <husnain@gmail.com>
2025-11-02 00:45:06 +0500

    Add Zoho Widget integration and initialization scripts

---
+++ app/zoho/page.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    if (window.ZOHO) {
+      ZOHO.embeddedApp.on("PageLoad", function(data) {
+        console.log("Widget Loaded", data);
+      });
+      ZOHO.embeddedApp.init();
+    }
+  }, []);
+
+  const getLead = async () => {
+    const res = await ZOHO.CRM.API.getRecord({
+      Entity: "Leads",
+      RecordID: "123456789"
+    });
+    console.log(res);
+  };
+
+  return (
+    <div>
+      <h1>Zoho Widget in Next.js</h1>
+      <button onClick={getLead}>Fetch Lead</button>
+    </div>
+  );
+}
---
+++ public/_app.jsx
@@ -0,0 +1,13 @@
+// _app.js
+import { useEffect } from 'react';
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://live.zwidgets.com/js-sdk/1.2/ZohoEmbededAppSDK.min.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return <Component {...pageProps} />;
+}
